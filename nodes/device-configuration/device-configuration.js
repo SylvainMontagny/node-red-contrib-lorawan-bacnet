6068500fc720d3abbe2a20261f0b96803844b27b
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-01-23 11:29:27 +0100

    create device-configuration

---
+++ nodes/device-configuration/device-configuration.js
@@ -0,0 +1,32 @@
+const tools = require("../shared/tools.js");
+
+module.exports = function (RED) {
+  function CreateDeviceObject(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    const nodeID = "ed250713f81379b7";
+    const nodeData = tools.findNode(nodeID);
+    if (!nodeData || !nodeData.func) {
+      node.error("No valid node found for the given ID");
+      return;
+    }
+    let nodeFunction;
+    try {
+      nodeFunction = new Function("msg", "node", "global", nodeData["func"]);
+    } catch (err) {
+      node.error("Failed to create dynamic function: " + err.message);
+      return;
+    }
+
+    node.on("input", function (msg) {
+      try {
+        const result = nodeFunction(msg, node, node.context().global);
+        node.send(result);
+      } catch (err) {
+        node.error("Error on Device Configuration: " + err.message, msg);
+      }
+    });
+  }
+
+  RED.nodes.registerType("device configuration", CreateDeviceObject);
+};
