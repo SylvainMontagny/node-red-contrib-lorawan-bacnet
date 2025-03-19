06806827dd35972d95d8c85d79c839334665301c
elias-qzo <116678729+elias-qzo@users.noreply.github.com>
2025-03-19 23:32:49 +0100

    Device configuration node rework

--- nodes/device-configuration/device-configuration.js
+++ nodes/device-configuration/device-configuration.js
@@ -1,16 +1,20 @@
 module.exports = function (RED) {
     function DeviceConfiguration(config) {
         RED.nodes.createNode(this, config);
-        this.list = config.list || [];
-
         var node = this;
-
+        
         node.on("input", function (msg) {
-            msg.payload = node.list.map(device => ({
-                name: device.name || "Unknown",
-            }));
+            let newDeviceList = {};
+            this.deviceList = config.deviceList || {};
+
+            node.deviceList.forEach(element => {
+                newDeviceList[element.deviceName] = { ...element };
+                delete newDeviceList[element.deviceName].deviceName;
+            });
+            msg.payload = newDeviceList;
             node.send(msg);
         });
+
     }
 
     RED.nodes.registerType("device configuration", DeviceConfiguration);
