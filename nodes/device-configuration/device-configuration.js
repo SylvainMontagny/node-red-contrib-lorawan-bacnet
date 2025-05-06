87cca59aaa665807396a0294acaa6ec839e3fa1d
elias-qzo <116678729+elias-qzo@users.noreply.github.com>
2025-05-07 01:50:09 +0200

    Clean up nodes

--- nodes/device-configuration/device-configuration.js
+++ nodes/device-configuration/device-configuration.js
@@ -2,83 +2,23 @@ module.exports = function (RED) {
     function DeviceConfiguration(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+
+        const global = node.context().global;
+        console.log(node.deviceList);
         
+        this.status({fill:"green",shape:"dot",text:"Configuration OK"});
+
+        global.set('g_deviceList', node.deviceList);
+        global.set('g_httpRequestTimeOut', 5000);
+        global.set('g_tts_topicDownlinkSuffix', "/down");
+        global.set('g_tts_topicUplinkSuffix', "/up");
+        global.set('g_chirp_topicDownlinkSuffix', "/command/down");
+        global.set('g_chirp_topicUplinkSuffix', "/event/up");
+        global.set('g_actility_topicDownlinkSuffix', "/downlink");
+        global.set('g_actility_topicUplinkSuffix', "/uplink");
+
         node.on("input", function (msg) {
             let deviceList = config.deviceList || {};
-
-            // Device name to device format
-            let tmpDeviceList = {};
-            deviceList.forEach(element => {
-                tmpDeviceList[element.deviceName] = { ...element };
-                delete tmpDeviceList[element.deviceName].deviceName;
-                // Change debug "none" to null
-                if(tmpDeviceList[element.deviceName].controller.debug[0] === "none"){
-                    tmpDeviceList[element.deviceName].controller.debug[0] = null;
-                }
-            });
-            deviceList = tmpDeviceList;
-
-            // Device objects array to json format
-            Object.values(deviceList).forEach((device) => {
-                let objectsArray = device.bacnet.objects;
-                delete device.bacnet.objectsCount;
-                device.bacnet.objects = {};
-                for (let i = 0; i < objectsArray.length; i++) {
-                    obj = objectsArray[i];
-                    let objectName = obj.objectName;
-                    delete obj.objectName;
-                    if(obj.dataDirection === "downlink") {
-                        switch (obj.downlinkStrategie) {
-                            case "compareToUplinkValue":
-                                delete obj.range;
-                                break;
-                            case "onChangeOfValueWithinRange":
-                                delete obj.uplinkToCompareWith;
-                                break;                 
-                            default:
-                                delete obj.uplinkToCompareWith;
-                                delete obj.range;
-                                break;
-                        }
-                    } else {
-                        delete obj.downlinkStrategie;
-                        delete obj.uplinkToCompareWith;
-                        delete obj.range;
-                    }
-                    device.bacnet.objects[objectName] = obj;
-
-                  }
-            });
-            
-            // Remove logins and passwords if protocol is not restAPIBacnet
-            // And add httpAuthentication if protocol is restAPIBacnet
-            if(config.protocol === "restAPIBacnet") {
-                for (let device in deviceList) {
-                    const buffer = Buffer.from(deviceList[device].controller.login + ':' + deviceList[device].controller.password);
-                    deviceList[device].controller.httpAuthentication = "Basic " + buffer.toString('base64');
-                }
-            } else {
-                for (let device in deviceList) {
-                    delete deviceList[device].controller.model;
-                    delete deviceList[device].controller.login;
-                    delete deviceList[device].controller.password;
-                }
-            }
-
-            // Remove actility key if networkServer is not actility
-            if(config.networkServer !== "actility") {
-                for (let device in deviceList) {
-                    delete deviceList[device].lorawan.actility;
-                }
-            }
-
-            // Remove chirpstack key if networkServer is not chirpstack
-            if(config.networkServer !== "chirpstack") {
-                for (let device in deviceList) {
-                    delete deviceList[device].lorawan.chirpstack;
-                }
-            }
-
             msg.payload = deviceList;
             node.send(msg);
         });
